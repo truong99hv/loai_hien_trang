a6118bc4ec1efb92c758fe86f4fcf329d9792fa1
Hoang <revloselbuort@gmail.com>
2023-05-17 23:51:26 +0700

    filter

--- src/SearchComponent/MainSearchComponent/mainSearch.js
+++ src/SearchComponent/MainSearchComponent/mainSearch.js
@@ -33,7 +33,7 @@ function RenderSideBarFunction({ tab }) {
     "loaihientrang_ids[]": [],
     "province_ids[]": [],
     "sach_do_ids[]": [],
-    "iucn_ids": [],
+    "iucn_ids[]": [],
   });
   const [currentPage, setCurrentPage] = useState(1);
   const [dataFilter, setDataFilter] = useState("");
@@ -160,128 +160,5 @@ function convertObject(object) {
   return result.join("");
 }
 
-// function GetData() {
-//   let [tab, setTab] = useState(0);
-//   let [data, setData] = useState([]);
-//   let [total, setTotal] = useState(0);
-//   let [displayItems, setDisplayItems] = useState(18);
-//   let [currentPage, setCurrentPage] = useState(1); //
-//   let [filterList, setFilterList] = useState({
-//     "loaihientrang_ids[]": [],
-//     "province_ids[]": [],
-//     "sach_do_ids[]": [],
-//     iucn_ids: [],
-//   });
-//   let [dataFilter, setDataFilter] = useState("");
-//   let onSetTab = (tab) => setTab(tab);
-//   let onsetPage = (e) => {
-//     e.preventDefault();
-//     setCurrentPage(currentPage + 1);
-//     setDisplayItems(displayItems + 18);
-//   }; //
-//   let onSetFilter = (control) => (e) => {
-//     const isChecked = e.target.checked;
-//     const value = e.target.value;
-//     if (isChecked) {
-//       setFilterList((prevFilterList) => ({
-//         ...prevFilterList,
-//         [control]: [...prevFilterList[control], value],
-//       }));
-//     } else {
-//       setFilterList((prevFilterList) => ({
-//         ...prevFilterList,
-//         [control]: prevFilterList[control].filter((item) => item !== value),
-//       }));
-//     }
-//   };
-//   let content = null;
-
-//   useEffect(() => {
-//     setDataFilter(convertObject(filterList));
-//     const returnData = async () => {
-//       try {
-//         const Data = await getDataFromApi(currentPage, dataFilter);
-//         const count = Data.pagination.total;
-//         setTotal(count);
-//         const newData = [];
-//         Data.list.forEach((item) => {
-//           let loai_hien_trang = null;
-//           let sach_dos = null;
-//           let iucns = null;
-//           let images = getRoute("domain") + "/static/img/image4.5aecb9b5.png";
-
-//           if (item.loai_hien_trang) {
-//             loai_hien_trang = item.loai_hien_trang.ten;
-//           }
-//           if (item.sach_dos.length > 0) {
-//             sach_dos = item.sach_dos[0].ma_danh_muc;
-//           }
-//           if (item.iucns.length > 0) {
-//             iucns = item.iucns[0].ma_danh_muc;
-//           }
-//           if (item.attachments.length > 0) {
-//             images = getRoute("domain") + item.attachments[0].path;
-//           }
-//           newData.push({
-//             name: item.ten,
-//             ten_khoa_hoc: item.ten_khoa_hoc,
-//             images: images,
-//             kingdom: item.kingdom.ten,
-//             phylumn: item.phylumn.ten,
-//             loai_hien_trang: loai_hien_trang,
-//             sach_dos: sach_dos,
-//             iucns: iucns,
-//           });
-//         });
-
-//         // setData([...data, ...newData]);
-
-//         // setData((prevData) => [...prevData, ...newData]);
-//         setData((prevData) => [...newData]);
-//       } catch (error) {
-//         console.log("ERROR: " + error);
-//       }
-//     };
-//     returnData();
-//     console.log(filterList);
-//   }, [currentPage, filterList, dataFilter]);
-
-//   switch (tab) {
-//     case 0:
-//       content = <GridView items={data} total = {total} />;
-//       break;
-//     case 1:
-//       content = <TableView items={data} />;
-//       break;
-//     default:
-//   }
-//   return (
-//     <>
-//       <RenderNavBar onClick={onSetTab} active={tab} />
-//       <RenderSideBar onSetFilter={onSetFilter} />
-//       <div className="main">
-
-//         {content}
-//         {displayItems < total && (
-//           <div className="buttonLoadMore">
-//             <a href="/" onClick={(e) => onsetPage(e)}>
-//               Tải thêm
-//             </a>
-//           </div>
-//         )}
-//       </div>
-//     </>
-//   );
-// }
-// function convertObject(object) {
-//   let result = [];
-//   for (let key in object) {
-//     object[key].forEach((item) => {
-//       result.push(`&${key}=${item}`)
-//       // result = "&" + result + key  + "="+ item;
-//     });
-//   }
-//   return result.join("");;
-// }
 
 export default MainSearch;
--- src/SearchComponent/SidebarComponent/sidebar.js
+++ src/SearchComponent/SidebarComponent/sidebar.js
@@ -2,21 +2,45 @@ import "./sidebar.css";
 import { AiFillQuestionCircle } from "react-icons/ai";
 import getDataFromApi from "../../DataHandle/getDataFromApi";
 import { useEffect, useState } from "react";
-function RenderSideBar({onSetFilter}) {
+function RenderSideBar({ onSetFilter }) {
   const [loaihientrang, setLoaihientrang] = useState([]);
+  const [redbook, setRedbook] = useState([]);
+  const [iucn, setIucn] = useState([]);
   useEffect(() => {
     const returnData = async () => {
       try {
-        const loaihientrang = await getDataFromApi("loaihientrang");
-        const newloaihientrang = []
-        loaihientrang.forEach((item) =>{
-            newloaihientrang.push({
-                id: item.id,
-                ten:item.ten,
-              });
-        })
-        setLoaihientrang(newloaihientrang);
-        console.log(newloaihientrang)
+        const loaihientrangList = await getDataFromApi("loaihientrang");
+        const newLoaihientrang = [];
+        loaihientrangList.forEach((item) => {
+          newLoaihientrang.push({
+            id: item.id,
+            ten: item.ten,
+          });
+        });
+        setLoaihientrang(newLoaihientrang);
+
+        const redbookList = await getDataFromApi("redbook");
+        const newRedbook = [];
+        redbookList[0].childs.forEach((item) => {
+          newRedbook.push({
+            id: item.id,
+            ten: item.ten,
+            ma_danh_muc: item.ma_danh_muc,
+          });
+        });
+        setRedbook(newRedbook);
+
+        const iucnList = await getDataFromApi("iucn");
+        const newiucnList = [];
+        iucnList[0].childs.forEach((item) => {
+          newiucnList.push({
+            id: item.id,
+            ten: item.ten,
+            ma_danh_muc: item.ma_danh_muc,
+          });
+        });
+        setIucn(newiucnList);
+        console.log(newiucnList);
       } catch (error) {
         console.log("ERROR: " + error);
       }
@@ -27,12 +51,49 @@ function RenderSideBar({onSetFilter}) {
     <>
       <div className="sidebarContainer">
         Loại
+        {console.log(1)}
         <AiFillQuestionCircle />
         <ul>
           <p>Hiện trạng loài</p>
-          {loaihientrang.length>0 && loaihientrang.map((item) => (
-            <li key={item.id}><input type="checkbox" value={item.id} onChange={onSetFilter("loaihientrang_ids[]")}/>{item.ten} </li>
-        ))}
+          {loaihientrang.length > 0 &&
+            loaihientrang.map((item) => (
+              <li key={item.id}>
+                <input
+                  type="checkbox"
+                  value={item.id}
+                  onChange={onSetFilter("loaihientrang_ids[]")}
+                />
+                {item.ten}
+              </li>
+            ))}
+        </ul>
+        <ul>
+          <p>Sách đỏ</p>
+          {redbook.length > 0 &&
+            redbook.map((item) => (
+              <li key={item.id}>
+                <input
+                  type="checkbox"
+                  value={item.id}
+                  onChange={onSetFilter("sach_do_ids[]")}
+                />
+                {item.ma_danh_muc} - {item.ten}
+              </li>
+            ))}
+        </ul>
+        <ul>
+          <p>IUCN</p>
+          {iucn.length > 0 &&
+            iucn.map((item) => (
+              <li key={item.id}>
+                <input
+                  type="checkbox"
+                  value={item.id}
+                  onChange={onSetFilter("iucn_ids[]")}
+                />
+                {item.ma_danh_muc} - {item.ten}
+              </li>
+            ))}
         </ul>
       </div>
     </>
